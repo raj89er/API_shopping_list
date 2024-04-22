c3a960c4eddf6bbe168b73fdb11b237910c48cde
raj89er <134079776+raj89er@users.noreply.github.com>
2024-04-21 21:06:48 -0400

    added {POST} User route

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,10 @@ from datetime import datetime, timezone
 from app import app
 from shopping_list import shopping_list
 
+
+users_list = []
+
+
 @app.route('/', methods=['GET'])
 def index():
     return '''
@@ -11,6 +15,43 @@ Hello There! ~General K.~
 Welcome to the Shopping List API!
 '''
 
+# User Endpoints
+@app.route('/users', methods=['POST'])
+def create_user():
+    # Error handling
+    if not request.is_json:
+        return {'error': 'Your content type must be in json'}, 400
+    
+    data = request.json
+    missing_fields = []
+    for field in ['firstName' , 'lastName' , 'username' , 'email' , 'password']:
+        if field not in data:
+            missing_fields.append(field)
+    if missing_fields:
+        return {'error': f'You are missing the following required fields: {", ".join(missing_fields)}'}, 400
+    
+    # Check if username or email already exists
+    for user in users_list:
+        if user['email'] == new_user['email'] or user['username'] == new_user['username']:
+            return {'error': 'User with that username and/or email already exists'}, 409
+    # Create a new user with the data received
+    new_user = {
+        'user_id': len(users_list) + 1,
+        'username': data.get('username'),
+        'name_first': data.get('description'),
+        'name_last': data.get('lastName'),
+        'email': data.get('email'),
+        'password': data.get('password'),
+        'date_joined': datetime.now(timezone.utc).strftime('%Y-%m-%d')
+        }
+    
+    # Add the new user to the users_list
+    users_list.append(new_user)
+    # Return a success message
+    return new_user, 201
+
+
+# Ingredients Endpoints
 @app.route('/ingredients', methods=['GET'])
 def get_ingredients():
     return {'ingredients': shopping_list}
@@ -25,18 +66,17 @@ def get_ingredient(ingredient_id):
 @app.route('/ingredients', methods=['POST'])
 def create_ingredient():
     # Error handling
-    if not request.json:
+    if not request.is_json:
         return {'error': 'Your content type must be in json'}, 400
     
     # Check for missing required fields (ingredient and description)
     data = request.json
     missing_fields = []
-    if 'ingredient' not in data:
-        missing_fields.append('ingredient')
-    if 'description' not in data:
-        missing_fields.append('description')
+    for field in ['ingredient', 'description']:
+        if field not in data:
+            missing_fields.append(field)
     if missing_fields:
-        return {'error': f'Missing required fields: {", ".join(missing_fields)}'}, 400
+        return {'error': f'You are missing the following required fields: {", ".join(missing_fields)}'}, 400
     
     ingredient_id = len(shopping_list) + 1  # Generate a new ingredient_id for the new task
     new_ingredient = {
@@ -46,6 +86,10 @@ def create_ingredient():
         'status': False,
         'date_added': datetime.now(timezone.utc).strftime('%Y-%m-%d')
         }
+    # Check if ingredient already exists
+    for ingredient in shopping_list:
+        if ingredient['ingredient'] == new_ingredient['ingredient']:
+            return {'error': 'Ingredient with that name already exists'}, 409
     # Add the new ingredient to the shopping_list
     shopping_list.append(new_ingredient)
     # Return a success message and the updated shopping_list
