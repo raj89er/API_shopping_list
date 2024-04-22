ac6b69786aac2077af0f9f9fd39d6848c9d8d34c
raj89er <134079776+raj89er@users.noreply.github.com>
2024-04-22 02:15:25 -0400

    updated methods

--- app/models.py
+++ app/models.py
@@ -9,6 +9,37 @@ class Ingredient(db.Model):
     description = db.Column(db.String, nullable=False)
     status = db.Column(db.Boolean, nullable=False, default=False)
     date_added = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def __repr__(self) -> str:
+        return f"<Ingredient {self.ingredient_id} | {self.ingredient}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self, **kwargs):
+        for field, value in kwargs.items():
+            if field in { 'ingredient', 'description', 'status' }:
+                setattr(self, field, value)
+        self.save()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def to_dict(self):
+        return {
+            'ingredient_id': self.ingredient_id,
+            'ingredient': self.ingredient,
+            'description': self.description,
+            'status': self.status,
+            'date_added': self.date_added
+        }
+
+
 
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
@@ -36,4 +67,14 @@ class User(db.Model):
     
     def check_password(self, plaintext_password):
         return check_password_hash(self.password, plaintext_password)
+    
+    def to_dict(self):
+        return {
+            'user_id': self.user_id,
+            'username': self.username,
+            'name_first': self.name_first,
+            'name_last': self.name_last,
+            'email': self.email,
+            'date_added': self.date_added
+        }
 
